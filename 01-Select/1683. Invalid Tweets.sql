24018a514f8fcd3573cf4d7756335385d7fa91eb
Anirudh C M <161741952+Anirudh1103@users.noreply.github.com>
2024-06-19 00:12:02 +0530

    Delete 01-Select directory

--- 01-Select/1683. Invalid Tweets.sql
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-select tweet_id from tweets where LENGTH(content) > 15
--- 01-Select/584. Find Customer Referee.sql
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-Select name from Customer where referee_id is null or referee_id != 2
